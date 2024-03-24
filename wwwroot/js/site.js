c2754273418df2b3f70c3b1b1eb2a69fb06ce24a
Robin Weitzel <robin.weitzel.rw@gmail.com>
2024-03-24 09:31:30 +0100

    improved navigation bar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const toggleMenu = () => {
+  const menu = document.getElementById('mobile-menu');
+
+  menu.classList.toggle('hidden');
+}
